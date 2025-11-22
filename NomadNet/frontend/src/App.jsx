894723c4116a0f8aff8613a66852cc65179a44c4
syedafatima-cloud <syedafatimafaisal07@gmail.com>
2025-11-22 21:03:36 +0500

    I don't remmber

--- NomadNet/frontend/src/App.jsx
+++ NomadNet/frontend/src/App.jsx
@@ -1,9 +1,9 @@
 // App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthPage from './pages/authPage';
-import ProfilePage from './pages/profilePage';
-import NotFoundPage from './pages/notFoundPage'; // 404 page
+import AuthPage from './pages/AuthPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage'; // 404 page
 import './App.css';
 
 function App() {
