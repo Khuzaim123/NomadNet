1aa67192f2ecf84ff264b6f0d02d236e3d90baea
Khuzaim Adnan Qureshi <khuzaimadnana@gmail.com>
2025-12-04 21:17:20 +0500

    Chat real Time

--- NomadNet/backend/src/controllers/conversationController.js
+++ NomadNet/backend/src/controllers/conversationController.js
@@ -1,4 +1,4 @@
-const Conversation = require('../models/conversation');
+const Conversation = require('../models/Conversation');
 const Message = require('../models/Message');
 const User = require('../models/User');
 
